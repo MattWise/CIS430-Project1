494bafa2dadd22cf24d6d381f4cfa98153302926
Matthew Wise <mwise@umassd.edu>
2017-02-08 19:03:52 -0500

    Created image processing module

---
+++ src/process_image.py
@@ -0,0 +1,11 @@
+import Image
+
+import argparse
+
+NUMBER_BINS=32 #Number of bins each
+SIZE_PIXEL=0.1 #Real size of one pixel, in square centimeters
+
+def resize_image(img_path,size,number_bins,size_pixel):
+    img=PIL.Image.open(img_path)
+
+if __name__ == '__main__':
