8562dfd6252eb9877b1d09132b9491cf8b5a4ab6
mwise <ttam.esiw@gmail.com>
2017-02-15 08:16:57 -0500

    Finished batch processing.

--- src/process_images.py
+++ src/process_images.py
@@ -1,4 +1,30 @@
-import numpy as np
+import os
 import argparse
-import csv
+import re
+import process_image
 
+size_re = re.compile(r'([0-9.]+)x([0-9.])+cm')
+def extract_size(filename):
+    width,height=map(float,size_re.match(filename).group(1,2))
+    return width,height
+
+def generate_output_name(path):
+    return os.path.splitext(path)[0]+'.basket'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Create .basket files from images for use with association rule data mining in Orange.")
+    parser.add_argument("images", type=str, nargs='+', help="Images to process")
+    parser.add_argument("-s", "--size-pixel", type=float, dest="size_pixel", default=process_image.SIZE_PIXEL,
+                        help="The desired size of a single pixel in square centimeters")
+    parser.add_argument("-n", "--number-bins", type=int, dest="number_bins", default=process_image.NUMBER_BINS,
+                        help="The desired number of bins per color")
+    args = parser.parse_args()
+
+    for image in args.images:
+        process_image.make_basket(img_path=image,
+                                  size=extract_size(image),
+                                  size_pixel=args.size_pixel,
+                                  number_bins=args.number_bins,
+                                  output_file=generate_output_name(image),
+                                  block_size=16)
